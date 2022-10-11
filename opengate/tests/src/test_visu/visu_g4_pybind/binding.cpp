1787d1d35b2d1986ff59b21f86da8b90e8f3b37b
David Sarrut <david.sarrut@creatis.insa-lyon.fr>
2022-10-11 18:15:41 +0200

    test with QBBC phys list

--- opengate/tests/src/test_visu/visu_g4_pybind/binding.cpp
+++ opengate/tests/src/test_visu/visu_g4_pybind/binding.cpp
@@ -4,7 +4,9 @@
 #include "G4UIExecutive.hh"
 #include "G4UImanager.hh"
 #include "G4RunManager.hh"
+#include "G4RunManagerFactory.hh"
 #include "G4VModularPhysicsList.hh"
+#include "G4EmStandardPhysics_option4.hh"
 #include "G4VUserPhysicsList.hh"
 #include "G4VisExecutive.hh"
 #include "G4VisManager.hh"
@@ -12,51 +14,97 @@
 #include "QBBC.hh"
 #include "DetectorConstruction.hh"
 #include "G4PhysListFactory.hh"
+#include "G4VPhysicsConstructor.hh"
 #include "G4VModularPhysicsList.hh"
 #include "G4String.hh"
 
 namespace py = pybind11;
 
 PYBIND11_MODULE(exampleB2, m) {
+
+    // G4UIExecutive
     py::class_<G4UIExecutive>(m, "G4UIExecutive")
         .def(py::init<>([]() {
             G4int argc = 1;
             char *argv[1];
-            argv[0] = (char*) malloc(strlen("qt")+1); // +1 for the terminator
-            strcpy(argv[0],"qt");
+            argv[0] = (char *) malloc(strlen("qt") + 1); // +1 for the terminator
+            strcpy(argv[0], "qt");
             return new G4UIExecutive(argc, argv);
         }), py::return_value_policy::reference)
         .def("SessionStart", &G4UIExecutive::SessionStart);
 
+    // G4VUserDetectorConstruction
     py::class_<G4VUserDetectorConstruction>(m, "G4VUserDetectorConstruction");
+
+    // DetectorConstruction
     py::class_<DetectorConstruction, G4VUserDetectorConstruction>(m, "DetectorConstruction")
-      .def(py::init(), py::return_value_policy::reference);
-    m.def("getDetectorConstruction", []() { return std::unique_ptr<G4VUserDetectorConstruction>(new DetectorConstruction); }, py::return_value_policy::reference);
+        .def(py::init(), py::return_value_policy::reference);
+    m.def("getDetectorConstruction",
+          []() { return std::unique_ptr<G4VUserDetectorConstruction>(new DetectorConstruction); },
+          py::return_value_policy::reference);
 
+    // G4VUserPhysicsList
     py::class_<G4VUserPhysicsList>(m, "G4VUserPhysicsList");
-    py::class_<G4VModularPhysicsList, G4VUserPhysicsList>(m, "G4VModularPhysicsList");
+
+    // G4VModularPhysicsList
+    py::class_<G4VModularPhysicsList, G4VUserPhysicsList>(m, "G4VModularPhysicsList")
+        .def("SetVerboseLevel", &G4VModularPhysicsList::SetVerboseLevel);
+
+    // G4PhysListFactory
     py::class_<G4PhysListFactory>(m, "G4PhysListFactory")
-      .def(py::init(), py::return_value_policy::reference)
-      .def("GetReferencePhysList", &G4PhysListFactory::GetReferencePhysList, py::return_value_policy::reference);
+        .def(py::init(), py::return_value_policy::reference)
+        .def("GetReferencePhysList", &G4PhysListFactory::GetReferencePhysList, py::return_value_policy::reference);
 
+    // G4VModularPhysicsList
+    // It is important to keep inheritance chain: G4VModularPhysicsList and G4VUserPhysicsList
+    py::class_<QBBC, G4VModularPhysicsList, G4VUserPhysicsList>(m, "QBBC")
+        .def(py::init(), py::return_value_policy::reference);
+
+    // G4VPhysicsConstructor
+    py::class_<G4VPhysicsConstructor>(m, "G4VPhysicsConstructor");
+
+    // G4EmStandardPhysics_option4
+    py::class_<G4EmStandardPhysics_option4, G4VPhysicsConstructor,
+        std::unique_ptr<G4EmStandardPhysics_option4, py::nodelete>>(m, "G4EmStandardPhysics_option4")
+        .def(py::init<G4int>(), py::return_value_policy::reference);
+
+    // G4RunManagerFactory
+    py::class_<G4RunManagerFactory, std::unique_ptr<G4RunManagerFactory, py::nodelete>>(m, "G4RunManagerFactory")
+        .def("CreateRunManager",
+             []() { return G4RunManagerFactory::CreateRunManager(G4RunManagerType::Default); },
+             py::return_value_policy::reference);
+
+    // G4RunManager
     py::class_<G4RunManager>(m, "G4RunManager")
-      .def(py::init(),py::return_value_policy::reference)
-      .def("Initialize", &G4RunManager::Initialize, py::return_value_policy::reference)
-      .def("SetUserInitialization", py::overload_cast<G4VUserPhysicsList*>(&G4RunManager::SetUserInitialization), py::return_value_policy::reference)
-      .def("SetUserInitialization", py::overload_cast<G4VUserDetectorConstruction*>(&G4RunManager::SetUserInitialization), py::return_value_policy::reference);
+        .def(py::init(), py::return_value_policy::reference)
+        .def("Initialize", &G4RunManager::Initialize, py::return_value_policy::reference)
+        .def("SetUserInitialization",
+             py::overload_cast<G4VUserPhysicsList *>(&G4RunManager::SetUserInitialization))
 
+        .def("SetUserInitialization",
+             py::overload_cast<G4VUserDetectorConstruction *>(&G4RunManager::SetUserInitialization));
+
+    // G4VVisManager
     py::class_<G4VVisManager>(m, "G4VVisManager");
+
+    // G4VisManager
     py::class_<G4VisManager, G4VVisManager>(m, "G4VisManager");
+
+    // G4VisExecutive
     py::class_<G4VisExecutive, G4VisManager>(m, "G4VisExecutive")
-      .def(py::init(), py::return_value_policy::reference)
-      .def("Initialize", &G4VisExecutive::Initialize, py::return_value_policy::reference);
+        .def(py::init(), py::return_value_policy::reference)
+        .def("Initialize", &G4VisExecutive::Initialize, py::return_value_policy::reference);
 
+    // G4UImanager
     py::class_<G4UImanager>(m, "G4UImanager")
-      .def("ApplyCommand", py::overload_cast<const G4String &>(&G4UImanager::ApplyCommand), py::return_value_policy::reference)
-      .def_static("GetUIpointer", &G4UImanager::GetUIpointer, py::return_value_policy::reference);
+        .def("ApplyCommand",
+             py::overload_cast<const G4String &>(&G4UImanager::ApplyCommand),
+             py::return_value_policy::reference)
+        .def_static("GetUIpointer", &G4UImanager::GetUIpointer, py::return_value_policy::reference);
 
+    // G4String
     py::class_<G4String>(m, "G4String")
-      .def(py::init<const std::string &>(), py::return_value_policy::reference);
+        .def(py::init<const std::string &>(), py::return_value_policy::reference);
 
 
 }
--- opengate/tests/src/test_visu/visu_g4_pybind/exampleB2.py
+++ opengate/tests/src/test_visu/visu_g4_pybind/exampleB2.py
@@ -2,32 +2,41 @@
 
 import exampleB2 as b2
 
-def visu_g4_pybind():
-    ui = b2.G4UIExecutive()
-    detectorConstruction = b2.getDetectorConstruction()
-    runManager = b2.G4RunManager()
-    runManager.SetUserInitialization(detectorConstruction)
-    physListName = b2.G4String("QBBC")
-    factory = b2.G4PhysListFactory()
-    physicsList = factory.GetReferencePhysList(physListName)
-    print(physicsList)
-    runManager.SetUserInitialization(physicsList)
+# UI: this will start qt interface
+ui = b2.G4UIExecutive()
 
-    visManager = b2.G4VisExecutive()
-    visManager.Initialize()
+# world
+detectorConstruction = b2.getDetectorConstruction()
 
-    UImanager = b2.G4UImanager.GetUIpointer();
+# Run Manager
+# no seg fault when G4RunManagerFactory is used instead of direct G4RunManager
+runManager = b2.G4RunManagerFactory.CreateRunManager()
+runManager.SetUserInitialization(detectorConstruction)
 
-    runManager.Initialize()
-    commandName = b2.G4String("/control/execute vis.mac")
-    UImanager.ApplyCommand(commandName);
-    print("Start visu")
-    ui.SessionStart();
+# PL
+physicsList = b2.QBBC()
+print('Physic list', physicsList)
+runManager.SetUserInitialization(physicsList)
 
-    del runManager
+# Vis
+visManager = b2.G4VisExecutive()
+visManager.Initialize()
 
-    print("done")
+# init
+runManager.Initialize()
 
-# -----------------------------------------------------------------------------
-if __name__ == '__main__':
-    visu_g4_pybind()
+# visu
+commandName = b2.G4String("/control/execute vis.mac")
+UImanager = b2.G4UImanager.GetUIpointer()
+UImanager.ApplyCommand(commandName)
+print("Start visu")
+
+# go
+ui.SessionStart()
+
+# del runManager
+# del physicsList
+# del detectorConstruction
+
+print()
+print("DONE ")
